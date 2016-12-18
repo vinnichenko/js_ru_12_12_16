60eb7b11bfd27436cd2bce82355c6b8099ea8a45
vinnichenko <vinnichenko.artem@gmail.com>
2016-12-18 12:35:20 +0200

    ht1: changing Comment component

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,15 +1,11 @@
 import React, { Component } from 'react'
 
-export default class Comment extends Component {
-
-    render() {
-        const { comment } = this.props;
-        return (
-            <div>
-                <h4 onClick = {this.toggleOpen}>{comment.user}</h4>
-                <p>{comment.text}</p>
-            </div>
-        )
-    }
-
+export default function Comment(props) {
+    const { comment } = props;
+    return (
+        <div>
+            <h4>{comment.user}</h4>
+            <p>{comment.text}</p>
+        </div>
+    )
 }
