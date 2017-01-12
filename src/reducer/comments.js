537d2f04dd3664774476e32635f6dbc141071047
vinnichenko <vinnichenko.artem@gmail.com>
2017-01-12 21:35:45 +0200

    ht5.1: CommentModel Record

--- src/reducer/comments.js
+++ src/reducer/comments.js
@@ -1,8 +1,15 @@
 import {  } from '../constants'
 import { normalizedComments } from '../fixtures'
 import { arrayToMap } from '../helpers'
+import { Record } from 'immutable'
 
-const defaultState = arrayToMap(normalizedComments)
+const CommentModel = Record({
+    "id": null,
+    "user": null,
+    "text": null
+})
+
+const defaultState = arrayToMap(normalizedComments, CommentModel)
 
 export default (state = defaultState, action) => {
     const { type, payload, response, error } = action
